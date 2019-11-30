fea7a549206c5686b1b5815fe2dddad06e304275
Chanelle <ckaith97@yahoo.com>
2019-11-30 13:26:31 +0100

    WIP dialogue system - not compiling

---
+++ GDD_2019_GROUP14/Assets/Dialogue/DialogueSystem.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{
+
+    public Canvas canvas;
+    public DialogueView dialoguePrefab;
+    public PageView pagePrefab;
+
+    public List<DialogueData> dialogues;
+
+    public Dictionary<string, DialogueData> dialogueLookup = new Dictionary<string, DialogueData>();
+
+    public Dialogue currentDialogue;
+
+    void Start() {
+
+        // Add dialogues to the dictionary
+        foreach (DialogueData dialogue in dialogues) {
+            dialogueLookup.Add(dialogue.id, dialogue);
+        }
+    }
+
+    public void BeginDialogue(string id) {
+        DialogueData dialogueData = dialogueLookup[id];
+        if (dialogueData == null) {
+            Debug.LogError("The requested dialogue, '" + id + "'  doesn't exist.");
+        }
+
+        // Create a new dialogue object and pass in the data
+        currentDialogue = new Dialogue(dialogueData);
+        PageData page = currentDialogue.Begin();
+
+        // Update GUI
+        currentDialogue.dialogueView = Instantiate(dialoguePrefab, canvas.transform);
+        currentDialogue.dialogueView.nextPage.onClick.AddListener( delegate {
+            NextPage(currentDialogue);
+        });
+
+        if (page is InfoPage) {
+
+        }
+        currentDialogue.pageView = Instantiate(pagePrefab, currentDialogue.dialogueView.panel.transform);
+        // currentDialogue.pageView.SetHeader(page.header);
+        // currentDialogue.pageView.SetBody(page.body); 
+    }
+
+    public void NextPage(Dialogue dialogue) {
+        if (dialogue == null) return;
+
+        // Get the next page data
+        PageData page = dialogue.NextPage();
+        if (page == null) {
+            CloseDialogue();
+        };
+
+        // Update GUI
+        Destroy(dialogue.pageView.gameObject);
+        dialogue.pageView = Instantiate(pagePrefab, dialogue.dialogueView.panel.transform);
+        // dialogue.pageView.SetHeader(page.header);
+        // dialogue.pageView.SetBody(page.body); 
+    }
+
+    public void CloseDialogue() {
+        if (currentDialogue == null) return;
+
+        Destroy(currentDialogue.pageView.gameObject);
+        Destroy(currentDialogue.dialogueView.gameObject);
+
+        currentDialogue = null;
+    }
+}
+
+[System.Serializable]
+public class Dialogue {
+
+    public DialogueData data;
+    
+    private int pageIndex;
+    public PageData currentPage;
+
+    public bool reachedEnd;
+
+    public DialogueView dialogueView;
+    public PageView pageView;
+
+    public Dialogue(DialogueData data) {
+        this.data = data;
+    }
+
+    public PageData Begin() {
+        if (data.pages.Count <= 0) {
+            Debug.LogError("There are no pages in this dialogue");
+            return null;
+        }
+
+        pageIndex = 0;
+        currentPage = data.pages[pageIndex];
+        return currentPage;
+    }
+
+    public PageData NextPage() {
+        pageIndex++;
+        if (pageIndex >= data.pages.Count) {
+            Debug.Log("Reached the end of dialogue, '" + data.id + "'");
+            reachedEnd = true;
+            return null;
+        }
+
+        currentPage = data.pages[pageIndex];
+        return currentPage;
+    } 
+}
+
---
+++ GDD_2019_GROUP14/Assets/Dialogue/PageView.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PageView : MonoBehaviour {
+    [SerializeField] private Text header;
+    [SerializeField] private Text body;
+
+    public void SetHeader(string str) {
+        if (header == null) {
+            Debug.LogError("Page is missing a header component");
+        }
+
+        header.text = str;
+    }
+
+    public void SetBody(string str) {
+        if (body == null) {
+            Debug.LogError("Page is missing a body component");
+        }
+
+        body.text = str;
+    }
+}
