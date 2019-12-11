01789973d6a456b294b62771c7c5aaf1e521138b
ania90210 <anna.ign6283@gmail.com>
2019-12-11 09:40:23 +0100

    Map/Enemy/Player

---
+++ GDD_2019_GROUP14/Assets/Scripts/Loader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+    public GameObject gameManager;			
+    
+    void Awake ()
+    {
+        //Check if a GameManager1 has already been assigned to static variable GameManager1.instance or if it's still null
+        if (GameManager1.instance == null)
+            Instantiate(gameManager);
+    }
+}
